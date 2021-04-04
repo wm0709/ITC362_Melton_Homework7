c55aceb4f6383643156cb731b1c78ac7647f674a
wm0709 <79418414+wm0709@users.noreply.github.com>
2021-04-04 12:23:12 -0500

    V0

---
+++ CandyStore/app/src/main/java/com/example/candystore/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.candystore;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflate the menu
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //Handle action bar items here
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.action_add:
+                Log.w("MainActivity", "Add Selected");
+                return true;
+            case R.id.action_delete:
+                Log.w("MainActivity", "Delete Selected");
+                return true;
+            case R.id.action_update:
+                Log.w("MainActivity", "Update Selected");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
